1d8728edd088243e2bdcb0ab32c4f5c3241912c0
olha_padalka_olya <olya.padalka@clockwise.software>
2018-11-08 14:58:10 +0200

    Add all files to repo

---
+++ routes/users.js
@@ -0,0 +1,58 @@
+const errors = require('restify-errors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const auth = require('../auth');
+const config = require('../config');
+
+module.exports = server => {
+  //Register user
+   server.post('/register', (req, res, next)=>{
+       const {email, password} = req.body;
+       const user = new User({
+           email,
+           password
+       });
+       bcrypt.genSalt(10, (err, salt)=>{
+           bcrypt.hash(user.password, salt, async (err, hash)=>{
+                //Hash passowrd
+                user.password = hash;
+
+                //Save user
+                try{
+                   const newUser = await user.save();
+                   res.send(201);
+                   next(); 
+                }catch(err){
+                    return next(new errors.InternalError(err.message));
+                }
+           });
+       });
+   });
+   
+   //Auth User
+   server.post('/auth', async (req, res, next)=>{
+       const {email, password} = req.body;
+       
+       next();
+
+       try{
+           //Auth User
+            const user = await auth.authenticate(email, password);
+            
+            //Create token
+            const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+                expiresIn: '15m'
+            });
+
+            const {iat, exp} = jwt.decode(token);
+            //Respond with token
+            res.send({iat, exp, token});
+
+       }catch(err){
+            //User unauthorized
+            return next(new errors.UnauthorizedError(err));
+       }
+
+   });
+};
